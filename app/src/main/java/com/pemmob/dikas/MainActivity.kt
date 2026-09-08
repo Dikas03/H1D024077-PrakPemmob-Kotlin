b5e94443215d3916c12bea638e89a7fcd19caa45
Dikas <dikasprd0@gmail.com>
2026-09-08 13:58:49 +0700

    Update MainActivity to match reference repo exactly

--- app/src/main/java/com/pemmob/dikas/MainActivity.kt
+++ app/src/main/java/com/pemmob/dikas/MainActivity.kt
@@ -53,7 +53,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
 @Composable
 fun LayoutTentangJualan(modifier: Modifier = Modifier) {
     Column(
